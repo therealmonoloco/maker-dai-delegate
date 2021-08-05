4ca1cd0997a6cad0a89230df441b3c2ce9b1d690
therealmonoloco <therealmonoloco@gmail.com>
2021-08-05 15:13:16 -0400

    feat: deposit to cdp and mint dai

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -13,6 +13,7 @@ import {
     Address
 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
+import "../interfaces/chainlink/AggregatorInterface.sol";
 import "../interfaces/maker/IMaker.sol";
 import "../interfaces/yearn/IVault.sol";
 
@@ -21,10 +22,22 @@ contract Strategy is BaseStrategy {
     using Address for address;
     using SafeMath for uint256;
 
+    uint256 internal constant WAD = 10**18;
+    uint256 internal constant RAY = 10**27;
+    uint256 internal constant RAD = 10**45;
+
     // CDP Manager
     // https://docs.makerdao.com/smart-contract-modules/proxy-module/cdp-manager-detailed-documentation
     ManagerLike internal constant cdpManager =
         ManagerLike(0x5ef30b9986345249bc32d8928B7ee64DE9435E39);
+
+    DaiJoinLike internal constant daiJoinAdapter =
+        DaiJoinLike(0x9759A6Ac90977b93B58547b4A71c78317f391A28);
+
+    // Jug accumulates stability fees for a particular collateral type
+    // https://docs.makerdao.com/smart-contract-modules/rates-module/jug-detailed-documentation
+    JugLike internal constant jug =
+        JugLike(0x19c0976f590D67707E62397C87829d896Dc0f1F1);
 
     // DAI yVault
     IVault public yVault = IVault(0xdA816459F1AB5631232FE5e97a05BBBb94970c95);
@@ -32,8 +45,18 @@ contract Strategy is BaseStrategy {
     // TODO: this should be part of the constructor to support different ilks.
     bytes32 public ilk = "YFI-A";
 
+    GemJoinLike internal constant gemJoinAdapter =
+        GemJoinLike(0x3ff33d9162aD47660083D7DC4bC02Fb231c81677);
+
+    // Use Chainlink oracle to obtain latest price
+    AggregatorInterface internal constant chainlinkYFItoUSDPriceFeed =
+        AggregatorInterface(0xA027702dbb89fbd58938e4324ac03B58d812b0E1);
+
     // Our vault identifier
     uint256 public cdpId;
+
+    // Our desired collaterization ratio
+    uint256 public collateralizationRatio;
 
     constructor(address _vault) public BaseStrategy(_vault) {
         // You can set these parameters on deployment to whatever you want
@@ -41,6 +64,8 @@ contract Strategy is BaseStrategy {
         // profitFactor = 100;
         // debtThreshold = 0;
         cdpId = cdpManager.open(ilk, address(this));
+        // Minimum collaterization ratio on YFI-A is 175%. Use 250% to be extra safe.
+        collateralizationRatio = 250;
     }
 
     // ******** OVERRIDE THESE METHODS FROM BASE CONTRACT ************
@@ -70,8 +95,14 @@ contract Strategy is BaseStrategy {
     }
 
     function adjustPosition(uint256 _debtOutstanding) internal override {
-        // TODO: Do something to invest excess `want` tokens (from the Vault) into your positions
-        // NOTE: Try to adjust positions so that `_debtOutstanding` can be freed up on *next* harvest (not immediately)
+        uint256 wantBalance = balanceOfWant();
+
+        if (wantBalance > _debtOutstanding) {
+            uint256 amountToDeposit = wantBalance.sub(_debtOutstanding);
+            _depositToCdp(amountToDeposit);
+        }
+
+        // TODO: check collateralization ratio to mint more dai or pay back dai
     }
 
     function liquidatePosition(uint256 _amountNeeded)
@@ -147,5 +178,114 @@ contract Strategy is BaseStrategy {
     {
         // TODO create an accurate price oracle
         return _amtInWei;
+    }
+
+    // ----------------- INTERNAL FUNCTIONS SUPPORT -----------------
+
+    function _checkAllowance(
+        address _contract,
+        IERC20 _token,
+        uint256 _amount
+    ) internal {
+        if (_token.allowance(address(this), _contract) < _amount) {
+            _token.safeApprove(_contract, 0);
+            _token.safeApprove(_contract, type(uint256).max);
+        }
+    }
+
+    function _getWantTokenPrice() internal view returns (uint256) {
+        int256 price = chainlinkYFItoUSDPriceFeed.latestAnswer();
+        require(price > 0); // dev: invalid price returned by chainlink oracle
+        return uint256(price);
+    }
+
+    function _depositToCdp(uint256 amount) internal {
+        if (amount == 0) {
+            return;
+        }
+
+        _checkAllowance(address(gemJoinAdapter), want, amount);
+
+        uint256 price = _getWantTokenPrice();
+        uint256 daiAmountToMint = amount.mul(price).div(collateralizationRatio);
+
+        address urn = cdpManager.urns(cdpId);
+        VatLike vat = VatLike(cdpManager.vat());
+
+        // Takes token amount from the strategy and joins into the vat
+        gemJoinAdapter.gem().transferFrom(
+            address(this),
+            address(gemJoinAdapter),
+            amount
+        );
+        gemJoinAdapter.join(urn, amount);
+
+        // Locks token amount into the CDP and generates debt
+        cdpManager.frob(
+            cdpId,
+            toInt(convertTo18(gemJoinAdapter, amount)),
+            _getDrawDart(vat, urn, daiAmountToMint)
+        );
+
+        // Moves the DAI amount (balance in the vat in rad) to the strategy
+        cdpManager.move(cdpId, address(this), toRad(daiAmountToMint));
+
+        // Exits DAI to the user's wallet as a token
+        vat.hope(address(daiJoinAdapter));
+        daiJoinAdapter.exit(msg.sender, daiAmountToMint);
+    }
+
+    // ----------------- INTERNAL CALCS -----------------
+
+    function balanceOfWant() internal view returns (uint256) {
+        return want.balanceOf(address(this));
+    }
+
+    // ----------------- UTILS FROM MAKERDAO DSS-PROXY-ACTIONS -----------------
+
+    function toInt(uint256 x) internal pure returns (int256 y) {
+        y = int256(x);
+        require(y >= 0, "int-overflow");
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x, "mul-overflow");
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "sub-overflow");
+    }
+
+    function toRad(uint256 wad) internal pure returns (uint256 rad) {
+        rad = mul(wad, 10**27);
+    }
+
+    function convertTo18(GemJoinLike gemJoin, uint256 amt)
+        internal
+        returns (uint256 wad)
+    {
+        // For those collaterals that have less than 18 decimals precision we need to do the conversion before passing to frob function
+        // Adapters will automatically handle the difference of precision
+        wad = mul(amt, 10**(18 - gemJoin.dec()));
+    }
+
+    function _getDrawDart(
+        VatLike vat,
+        address urn,
+        uint256 wad
+    ) internal returns (int256 dart) {
+        // Updates stability fee rate
+        uint256 rate = jug.drip(ilk);
+
+        // Gets DAI balance of the urn in the vat
+        uint256 dai = vat.dai(urn);
+
+        // If there was already enough DAI in the vat balance, just exits it without adding more debt
+        if (dai < mul(wad, RAY)) {
+            // Calculates the needed dart so together with the existing dai in the vat is enough to exit wad amount of DAI tokens
+            dart = toInt(sub(mul(wad, RAY), dai) / rate);
+            // This is neeeded due to lack of precision. It might need to sum an extra dart wei (for the given DAI wad amount)
+            dart = mul(uint256(dart), rate) < mul(wad, RAY) ? dart + 1 : dart;
+        }
     }
 }

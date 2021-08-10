e4a1712119dcb3450aa9268c36eb944df06cff96
therealmonoloco <therealmonoloco@gmail.com>
2021-08-10 15:59:05 -0400

    test: add revoke with profit

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -31,3 +31,21 @@ def test_revoke_strategy_from_strategy(
     chain.sleep(1)
     strategy.harvest()
     assert pytest.approx(token.balanceOf(vault.address), rel=RELATIVE_APPROX) == amount
+
+
+def test_revoke_with_profit(
+    token, vault, strategy, token_whale, gov, borrow_token, borrow_whale, yvault,
+):
+    token.approve(vault, 2 ** 256 - 1, {"from": token_whale})
+    vault.deposit(20 * (10 ** token.decimals()), {"from": token_whale})
+    strategy.harvest({"from": gov})
+
+    # Send some profit to yvault
+    borrow_token.transfer(
+        yvault, 20_000 * (10 ** borrow_token.decimals()), {"from": borrow_whale}
+    )
+    vault.revokeStrategy(strategy, {"from": gov})
+    strategy.harvest({"from": gov})
+
+    assert vault.strategies(strategy).dict()["totalGain"] > 0
+    assert vault.strategies(strategy).dict()["totalDebt"] <= 1
